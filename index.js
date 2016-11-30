0a1b7d602a492735d1bc95bd7ef300dc09eadbdb
Joren l'Ami <joren@distinct.nl>
2016-11-30 22:56:21 +0100

    combination of Funnel and stew

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 /* jshint node: true */
 'use strict';
 
+var stew = require('broccoli-stew');
+var Funnel = require('broccoli-funnel');
+var path = require('path');
+let debug = require('debug');
+debug.enable('broccoli-stew:*');
+
 module.exports = {
-  name: 'addon-es6'
+  name: 'addon-es6',
+  
+  treeForAddon() {
+  	let modES6 = stew.find(require.resolve('npm-es6'));//, { files: ['index.js'] });
+//  	modES6 = stew.rename(modES6, () => path.join('modules',/* 'npm-es6',*/ 'npm-es6.js'));
+  	modES6 = Funnel(modES6, {
+  		destDir: 'modules/npm-es6/',
+  		getDestinationPath: () => path.join(/*'modules', 'npm-es6',*/ 'index.js')
+  	});
+  	
+  	let root = this._super.apply(this, arguments);
+  	
+  	let res = stew.find([root, modES6]);
+  	
+  	res = stew.log(res);
+  	
+  	return res;
+  },
+  
+  isDevelopingAddon() {
+  	return true;
+  },
 };
